22f933a445c7d5ebe5e17499ccc220c336c03f5a
Dho99 <ridhoawwaludin@gmail.com>
2025-05-09 06:23:38 +0700

    fix searchParams issue

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,11 +1,14 @@
 // app/auth/error/page.tsx
+import { Suspense } from 'react';
 import ClientErrorPage from './ClientErrorPage';
 
 export default function ErrorPage() {
   return (
     <div>
       <h1>Error</h1>
-      <ClientErrorPage />
+      <Suspense fallback={<p>Loading error message...</p>}>
+        <ClientErrorPage />
+      </Suspense>
     </div>
   );
 }
