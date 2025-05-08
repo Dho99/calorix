2784cf87bb56d150a3538f7163d4e9016f8feeec
Dho99 <ridhoawwaludin@gmail.com>
2025-05-09 06:04:36 +0700

    add prisma generated client

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -433,7 +433,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
