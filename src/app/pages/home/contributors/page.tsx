e6e76ecd788d8f9247ce34c01a08744c2c5cb6dc
Dho99 <ridhoawwaludin@gmail.com>
2025-05-18 11:46:56 +0700

    fix NaN and contributors

--- src/app/pages/home/contributors/page.tsx
+++ src/app/pages/home/contributors/page.tsx
@@ -39,10 +39,10 @@ export default function Page() {
   return (
     <div className="w-full h-full flex flex-col justify-between dark:text-white relative py-20 items-center">
       <h1 className="text-4xl font-bold text-center">Contributors</h1>
-      <div className="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-10 mt-10">
+      <div className="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 gap-10 mt-10 lg:w-full w-3/4 lg:px-10 px-5">
         {
           contributors.map((contributor, index) => (
-            <div key={index} className="flex flex-col items-center bg-black/20 bg-white/20 p-5 rounded-lg shadow-lg transition-transform transform hover:scale-105">
+            <div key={index} className={`flex flex-col items-center bg-black/20 bg-white/20 p-5 rounded-lg shadow-lg transition-transform transform hover:scale-105 w-full ${index === 4 ? "lg:col-span-1 md:col-span-2 col-span-1" : ""}`}>
               <Image src={`/assets/static/contributors/${contributor.image}`} alt={contributor.name} className="w-34 h-42 rounded-full border border-2 border-black/20 dark:border-white/30 mb-4" width={500} height={500} quality={100}/>
               <h2 className="text-lg font-semibold">{contributor.name}</h2>
               <p className="text-gray-500 dark:text-white/90">{contributor.role}</p>
